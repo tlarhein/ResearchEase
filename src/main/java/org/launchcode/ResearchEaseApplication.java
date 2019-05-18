3bce965ab3267ad6b1f58e1d28f88772e6634995
tlarhein <tlarhein@gmail.com>
2019-05-18 18:03:38 -0500

    Added Hello/welcome and setup invitation controllers (commit has errors)

--- src/main/java/org/launchcode/ResearchEaseApplication.java
+++ src/main/java/org/launchcode/ResearchEaseApplication.java
@@ -1,13 +1,15 @@
 package org.launchcode;
+//Program by Tracey Cannon, LaunchCode Liftoff//
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ResearchEaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResearchEaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchEaseApplication.class, args);
+    }
 
 }
