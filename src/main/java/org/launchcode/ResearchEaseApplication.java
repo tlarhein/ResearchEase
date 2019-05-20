5ad4d0c9cbacf5681fd48469a50d364be9045a05
tlarhein <tlarhein@gmail.com>
2019-05-19 22:16:06 -0500

    Revised controller and application

--- src/main/java/org/launchcode/ResearchEaseApplication.java
+++ src/main/java/org/launchcode/ResearchEaseApplication.java
@@ -1,8 +1,10 @@
 package org.launchcode;
-//Program by Tracey Cannon, LaunchCode Liftoff//
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Program by Tracey Cannon, LaunchCode Liftoff//
 
 
 @SpringBootApplication
